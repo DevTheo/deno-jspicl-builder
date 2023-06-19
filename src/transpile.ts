0fbfb94d3d74355192e77a98f2caa0441cca9c5e
Jay Kimble <jkimble@gmail.com>
2023-06-18 20:36:53 -0400

    Initial port

---
+++ src/transpile.ts
@@ -0,0 +1,21 @@
+// deno-lint-ignore-file no-explicit-any
+import {jspicl} from "https://raw.githubusercontent.com/DevTheo/deno-jspicl/v3.0.1pre2/src/mod.ts";
+import { JspiclPluginOptions, banner } from "./constants.ts";
+
+
+export function transpile (javascriptCode: string, options: JspiclPluginOptions) {
+  const { includeBanner, polyfillTransform, jspicl: jspiclOptions = {} } = options;
+  const jspiclBanner = includeBanner && `${banner}` || "";
+
+  const { code, polyfills } = jspicl(javascriptCode, jspiclOptions);
+  const polyfillOutput = polyfillTransform ? polyfillTransform(polyfills) : Object.values(polyfills).join("\n");
+  const lua = `${polyfillOutput}${code}`;
+
+  return {
+    lua,
+    polyfillOutput,
+    toString () {
+      return `${jspiclBanner}${lua}`;
+    }
+  };
+}
